9cfa333bb4a9c3f5c63e438784afa3288f4210af
Mustafa <mustafamcm9@gmail.com>
2023-01-19 23:30:49 +0300

    final commit

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const menuIcon = document.getElementById('menu-icon');
 const closeIcon = document.getElementById('close-icon');
 const modal = document.getElementById('modal');
-const menuBar = document.getElementById('menu-bar')
-const mockups = document.getElementById('mockups')
+const menuBar = document.getElementById('menu-bar');
+const mockups = document.getElementById('mockups');
+const header = document.querySelector('header');
 
 menuIcon.addEventListener('touchend',() => {
-  modal.classList.toggle('hidden');
-  mockups.classList.toggle('hidden');
-  menuBar.classList.toggle('invisible');
-  closeIcon.classList.toggle('hidden');
-  menuIcon.classList.toggle('invisible')
+  menu();
 })
 
 closeIcon.addEventListener('touchend', () => {
+  menu();
+})
+
+function menu() {
   modal.classList.toggle('hidden');
   mockups.classList.toggle('hidden');
   menuBar.classList.toggle('invisible');
   closeIcon.classList.toggle('hidden');
-  menuIcon.classList.toggle('invisible')
-})
+  menuIcon.classList.toggle('invisible');
+  header.classList.toggle('relative');
+  header.classList.toggle('fixed');
+}
